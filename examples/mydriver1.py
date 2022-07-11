0e7be0d884892cbf69cf7688a034c9b7534d9dff
BBarel <bbarel80@gmail.com>
2022-07-11 15:19:01 +0300

    first driver drive fuction

--- examples/mydriver1.py
+++ examples/mydriver1.py
@@ -7,4 +7,20 @@ driver_name = "Amit"
 
 
 def drive(world):
+    x = world.car.x
+    y = world.car.y
+    obstacle = world.get((x, y - 1))
+    if world.get((x, y - 1)) == obstacles.PENGUIN:
+        return actions.PICKUP
+    elif obstacle == obstacles.WATER:
+        return actions.BRAKE
+    elif obstacle == obstacles.TRASH:
+        return actions.LEFT
+    elif obstacle == obstacles.CRACK:
+        return actions.JUMP
+    elif obstacle == obstacles.BIKE:
+        return actions.RIGHT
+    elif obstacle == obstacles.BARRIER:
+        return actions.RIGHT
+
     return actions.NONE
