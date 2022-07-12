91fc6ca40270d23cbe03d7b7942ffc909569cc13
BBarel <bbarel80@gmail.com>
2022-07-12 11:47:03 +0300

    bug fixing

--- examples/mydriver1.py
+++ examples/mydriver1.py
@@ -134,7 +134,7 @@ def way_to_actions(way):
 
 
 def drive(world):
-    # global right_balance, action_list, cnt
+    global right_balance, action_list, cnt
     res = actions.NONE
 
     x = world.car.x
@@ -146,13 +146,13 @@ def drive(world):
         res = actions.BRAKE
     elif obstacle == obstacles.CRACK:
         res = actions.JUMP
-    # else:
-    #     if cnt == 3:
-    #         cnt = 0
-    #     if cnt == 0:
-    #         score_board = world_to_score_board(world, right_balance)
-    #         action_list = way_to_actions(best_way(world, score_board, right_balance))
-    #         res = action_list[0]
+    else:
+        if cnt == 3:
+            cnt = 0
+        if cnt == 0:
+            score_board = world_to_score_board(world, right_balance)
+            action_list = way_to_actions(best_way(world, score_board, right_balance))
+            res = action_list[0]
     #     elif 0 < cnt < 3:
     #         res = action_list[cnt]
     # cnt += 1
