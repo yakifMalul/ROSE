d6ad52c55bff4ed8ee92d7032f58d3e3197c6741
BBarel <bbarel80@gmail.com>
2022-07-12 13:59:53 +0300

    bug fixing #4795

--- examples/mydriver1.py
+++ examples/mydriver1.py
@@ -150,13 +150,13 @@ def drive(world):
     elif obstacle == obstacles.CRACK:
         res = actions.JUMP
     else:
-        if cnt >= num_of_steps:
+        if cnt >= num_of_steps - 1:
             cnt = 0
         if cnt == 0:
             score_board = world_to_score_board(world)
             action_list = way_to_actions(best_way(world, score_board))
             res = action_list[0]
-        elif 0 < cnt < num_of_steps:
+        elif 0 < cnt < num_of_steps - 1:
             res = action_list[cnt]
     cnt += 1
 
