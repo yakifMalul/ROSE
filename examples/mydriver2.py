91230d99e470115844e62262542007c2002c098e
BBarel <bbarel80@gmail.com>
2022-07-12 18:05:25 +0300

    bug fixing #4814

--- examples/mydriver2.py
+++ examples/mydriver2.py
@@ -7,4 +7,15 @@ driver_name = "Yakir"
 
 
 def drive(world):
-    return actions.NONE
+    res = actions.NONE
+    x = world.car.x
+    y = world.car.y
+    obstacle = world.get((x, y - 1))
+    if world.get((x, y - 1)) == obstacles.PENGUIN:
+        res = actions.PICKUP
+    elif obstacle == obstacles.WATER:
+        res = actions.BRAKE
+    elif obstacle == obstacles.CRACK:
+        res = actions.JUMP
+
+    return res
